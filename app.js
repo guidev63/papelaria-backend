5580e4031b9c7ae4dd29075e7c20f93183463332
Informática para Internet <devinfo@sistemafieto.rede>
2024-02-21 21:39:40 -0300

    iniciamos a rota / thunder client

--- app.js
+++ app.js
@@ -1,9 +1,61 @@
 const express = require('express')
 const app = express();
+const usuario =[
+    {
+    id:1,
+    nome:"Bleno",
+    email:"bleno@gmail.com",
+    senha:"123",
+    },
+    {
+    id:2,
+    nome:"felipe",
+    email:"felipe@gmail.com",
+    senha:"123",
+    },
+    {
+    id:3,
+    nome:"nero",
+    email:"nero@gmail.com",
+    senha:"123",
+    },
+    {
+    id:4,
+    nome:"carlinhos",
+    email:"carlinhos@gmail.com",
+    senha:"123",
+    }
 
-app.use((req,res,next)=>{
-    res.json({
-        mensagem:"Hello Word!"
-    })
+    
+]
+app.get("/",(req,res,next)=>{
+    res.json(usuario)
 })
+app.get("/usuario",(req,res,next)=>{
+    let nomes=[];
+    usuario.map((linha)=>{
+       nomes.push({
+         nome:linha.nome,
+         email:linha.email
+       })
+    })
+
+    res.json(nomes)
+})
+app.post("/usuario",(req,res,next)=>{
+   const id = req.body.id
+   const nome = req.body.nome;
+   const email = req.body.email;
+   const senha = req.body.senha;
+   const dados = [{
+    id,
+    nome,
+    email,
+    senha
+
+   }]
+   console.log(dados);
+});
+
+
 module.exports = app
